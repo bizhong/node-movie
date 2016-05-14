a059ec303a2aa25486b1fcc1edfc5c0a3876b55b
bizhongbio <bizhongbio@outlook.com>
2016-05-14 16:51:58 +0800

    show movies on homepage

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,13 @@
+// 加载电影模型、电影分类模型、电影评论模型
+var Movie = require('../models/movie.js');
+
+// 首页
 exports.index = function(req, res) {
     // console.log("Cookies: ", req.cookies);
-    
-    res.render('index', {
-        title: '电影'
+    Movie.find({}, function(err, movies) {
+        res.render('index',{
+            title: '电影',
+            movies: movies
+        });
     });
 };
